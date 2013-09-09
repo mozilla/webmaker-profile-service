fca0f12ad3933b7f4408fb574a7c1ffbb446f676
Gvn Suntop <gavin@gsuntop.com>
2013-09-09 15:56:35 -0700

    fake user data

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-var http = require('http');
+var restify = require('restify');
+var fs = require('fs');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'content-type': 'text/plain'});
-  res.end('Hello, World!\n');
-}).listen(8142);
+var server = restify.createServer({
+  name: 'webmaker-profile-service',
+  version: '1.0.0'
+});
+
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+server.get('/user-data/:username', function (req, res, next) {
+  var userData = fs.readFileSync('fake.json', {
+    encoding: 'utf8'
+  });
+
+  userData = JSON.parse(userData);
+
+  res.send(userData);
+  return next();
+});
+
+server.listen(8080, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
